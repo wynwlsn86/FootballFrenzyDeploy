b871a6997857c2482bad6aa33e9a38e94e9c30b3
Wayne Wilson <wynwlsn86@gmail.com>
2019-06-16 19:45:06 -0400

    updated databse_url

--- client/src/services/apiServices.js
+++ client/src/services/apiServices.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-const URL = 'http://localhost:4567'
+const DATABASE_URL = `postgres://swlyejybejafoh:30967297f66bc46f00e71a5d0acd8b9ba41ff6d48483dd06e1eff255621359fc@ec2-107-22-238-217.compute-1.amazonaws.com:5432/dfvcbfi26j48i7`
 
 export const fetchAllUsers = async () => {
   try {
-    const allUsers = await axios.get(`${URL}/leagues/1/team/1/users/1`)
+    const allUsers = await axios.get(`${DATABASE_URL}/leagues/1/team/1/users/1`)
     //change the last 1 to a refrence to the usr passed dow
     return allUsers.data
   }
@@ -12,16 +12,16 @@ export const fetchAllUsers = async () => {
   }
 }
 export const fetchLeagueTeams = async (league_id) => {
-  const leagueTeams = await axios.get(`${URL}/teams/1/leagues/${league_id}`)
+  const leagueTeams = await axios.get(`${DATABASE_URL}/teams/1/leagues/${league_id}`)
   return leagueTeams.data
 }
 export const login = async (params) => {
   try{
-    const login = await axios.get(`${URL}/leagues/1/team/1/users`)
+    const login = await axios.get(`${DATABASE_URL}/leagues/1/team/1/users`)
     let user = login.data.find(user => {
       return user.email == params.email})
     let userId = user.id
-    const userData = await axios.get(`${URL}/leagues/1/team/1/users/${userId}`)
+    const userData = await axios.get(`${DATABASE_URL}/leagues/1/team/1/users/${userId}`)
     return userData.data
   }
   catch(e){
@@ -30,7 +30,7 @@ export const login = async (params) => {
 
 export const fetchAllTeams = async () => {
   try{
-    const allTeams = await axios.get(`${URL}/players/1/teams`)
+    const allTeams = await axios.get(`${DATABASE_URL}/players/1/teams`)
     return allTeams.data
   }
   catch(e){
@@ -40,7 +40,7 @@ export const fetchAllTeams = async () => {
 
 export const fetchTeam = async (team_id) => {
   try{
-    const team = await axios.get(`${URL}/players/1/teams/${team_id}`)
+    const team = await axios.get(`${DATABASE_URL}/players/1/teams/${team_id}`)
     console.log('single team fetch')
     return team.data
   }
@@ -51,7 +51,7 @@ export const fetchTeam = async (team_id) => {
 
 export const fetchAllPlayers = async () => {
   try {
-    const allPlayers = await axios.get(`${URL}/players`)
+    const allPlayers = await axios.get(`${DATABASE_URL}/players`)
     return allPlayers.data
   }
   catch(e){
@@ -70,7 +70,7 @@ export const fetchAllPlayers = async () => {
 // }
 export const updatePlayersTeam = async (player_id, params) => {
   try{
-    const player = await axios.put(`${URL}/players/${player_id}`, params)
+    const player = await axios.put(`${DATABASE_URL}/players/${player_id}`, params)
     console.log('player updated')
     return player.data
   }
@@ -80,7 +80,7 @@ export const updatePlayersTeam = async (player_id, params) => {
 }
 export const updateTeam = async (team_id, params) => {
   try{
-    const team = await axios.put(`${URL}/players/1/teams/${team_id}`, params)
+    const team = await axios.put(`${DATABASE_URL}/players/1/teams/${team_id}`, params)
     console.log('position updated')
     console.log('************************')
     return team.data
@@ -92,7 +92,7 @@ export const updateTeam = async (team_id, params) => {
 
 export const deleteTeam = async (id) => {
   try{
-    const deleting = await axios.delete(`${URL}/players/1/teams/${id}`)
+    const deleting = await axios.delete(`${DATABASE_URL}/players/1/teams/${id}`)
     return deleting.data
   }
   catch(e){
@@ -103,7 +103,7 @@ export const deleteTeam = async (id) => {
 
 export const createTeam = async (params) => {
   try{
-    const newTeam = await axios.post(`${URL}/players/1/teams/`, params)
+    const newTeam = await axios.post(`${DATABASE_URL}/players/1/teams/`, params)
     console.log('teamcreated')
     return newTeam.data
   }
@@ -114,7 +114,7 @@ export const createTeam = async (params) => {
 
 export const fetchPlayer = async(player_id) => {
   try{
-    const player = await axios.get(`${URL}/players/${player_id}`)
+    const player = await axios.get(`${DATABASE_URL}/players/${player_id}`)
     console.log('sinple player fetch')
   }
   catch(e){
